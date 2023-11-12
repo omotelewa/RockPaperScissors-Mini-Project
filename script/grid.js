d7858c314355f7ef2013e8fa9bbe74f5656fad05
marian <rahman.marian@gmail.com>
2023-11-12 07:32:15 -0500

    Added snakes and ladders to the games board and update the grids.js, games.js, ladder.js and index.html.

--- script/grid.js
+++ script/grid.js
@@ -1,21 +1,29 @@
 class Grid {
-      constructor() {
+  constructor() {
     this.grid = [];
     let value = 100;
     for (let i = 10; i > 0; i--) {
       let row = [];
       for (let j = 10; j > 0; j--) {
-        row.push(value)
-        value --
-
+        row.push(value);
+        value--;
       }
       this.grid.push(row);
     }
     this.snakes = {
-
+      90: new Snake(38),
+      74: new Snake(35),
     };
     this.ladders = {
-
+      67: new Ladder(86),
+      51: new Ladder(64),
+      14: new Ladder(36),
     };
+  }
+  checkSnake(location) {
+    if (location in this.snakes) {
+      return this.snakes[location].tailLocation;
+    }
+    return -1;
   }
 }
--- script/ladder.js
+++ script/ladder.js
@@ -1,5 +1,5 @@
             
-class ladders{
+class Ladder{
     //Snake is a class that represents the snake in our game. It has attributes for its length, direction of movement and position on the grid
     constructor (tailLocation){
         this.tailLocation = tailLocation;
